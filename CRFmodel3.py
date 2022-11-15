29e25830ca631e9e643cc62199d9fda5f95105b5
Changhyun Lee <changhyunlee.p@gmail.com>
2022-11-14 20:37:31 -0500

    Update CRFmodel3.py

--- CRFmodel3.py
+++ CRFmodel3.py
@@ -47,9 +47,10 @@ class CRFModel(nn.Module):
         
         # mask is used to avoid/ignore padded values of the input tensor
         # masking indices should be {0: if padded, 1: if not padded}
-        inputIds[inputIds==self.padValue] = 1
-        inputIds[inputIds!=self.padValue] = 0
-        mask = inputIds
+        # inputIds[inputIds==self.padValue] = 0 #0
+        # inputIds[inputIds!=self.padValue] = 1 #1
+        # mask = inputIds
+        mask = 1 - (inputIds == (self.padValue)).long()
         #changed 7:29pm
         
 
