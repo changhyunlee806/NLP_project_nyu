7ed34a678e5831fc760994e83318aca10b7640c4
yjlee0905 <yjeonlee0905@gmail.com>
2022-11-17 19:52:48 -0500

    WIP: merge

--- temp/CRFmodel.py
+++ temp/CRFmodel.py
@@ -22,33 +22,52 @@ class CRFModel(nn.Module):
 
     def forward(self, sentences, sentencesMask, speakerIds, lastTurns, emotionIdxes=None):
     #def forward(self, sentences, sentencesMask, speakerIds, emotionIdxes):
-        batchSize, maxTurns = sentences.shape[0], sentences.shape[1]
+        # works
+        sentBatchSize, sentMaxTurns, sentMaxLen = sentences.shape[0], sentences.shape[1], sentences.shape[2]
+        speakerBatchSize, speakerMaxTurns = speakerIds.shape[0], speakerIds.shape[1]
 
-        speakerIdsReshaped = speakerIds.reshape(batchSize * maxTurns, -1)
-        sentencesReshaped = sentences.reshape(batchSize * maxTurns, -1)
+        sentInputRowNum = sentBatchSize * sentMaxTurns
+        speakerInputRowNum = speakerBatchSize * speakerMaxTurns
 
-        clsId = torch.ones_like(speakerIdsReshaped) * self.CLS
-        inputIds = torch.cat((clsId, sentencesReshaped), 1)
-        mask = 1 - (inputIds == (self.padValue)).long()
+        sentencesReshaped = sentences.reshape(sentInputRowNum, -1)
+        speakerIdsReshaped = speakerIds.reshape(speakerInputRowNum, -1)  # changed 6:42pm
+        # works
+
+        # changed
+        clsId = torch.ones(speakerIdsReshaped.size(), dtype=speakerIdsReshaped.dtype, \
+                           layout=speakerIdsReshaped.layout, device=speakerIdsReshaped.device) * self.CLS
+        inputIds = torch.concat(tensors=(clsId, sentencesReshaped), dim=1)
+        # inputIds = torch.cat((clsId, sentencesReshaped), 1)
+        # mask = 1 - (inputIds == (self.padValue)).long()
+
+        # mask is used to avoid/ignore padded values of the input tensor
+        # masking indices should be {0: if padded, 1: if not padded}
+        attentionMask = torch.where(inputIds == self.padValue, 0, 1)
+        # changed 7:29pm
 
         utteranceEncoded = self.encoder(
             input_ids=inputIds,
-            attention_mask=mask,
+            attention_mask=attentionMask,
             output_hidden_states=True,
             return_dict=True
         )['last_hidden_state']
 
-        maskPos = mask.sum(1)-2
+        maskPos = torch.sum(input=attentionMask, dim=1, keepdim=False) - 2
+        # change below
         features = utteranceEncoded[torch.arange(maskPos.shape[0]), maskPos, :]
         emissions = self.emission(features)
-        crfEmissions = emissions.reshape(batchSize, maxTurns, -1).transpose(0, 1)
-        sentencesMask = sentencesMask.transpose(0, 1)
-        speakerIds = speakerIds.reshape(batchSize, maxTurns).transpose(0, 1)
-        lastTurns = lastTurns.transpose(0, 1)
+        crfEmissions = emissions.reshape(sentBatchSize, sentMaxTurns, -1).transpose(0, 1)
+
+        sentencesMask = torch.transpose(sentencesMask, dim0=0, dim1=1)
+        # check if it runs, if not it may mean speaker and sentence batch size are different
+        speakerIds = torch.transpose(speakerIds.reshape(speakerBatchSize, speakerMaxTurns), dim0=0,
+                                     dim1=1)  # 6:55pm changed from speakerBatchSize to sentBatchSize
+        lastTurns = torch.transpose(lastTurns, dim0=0, dim1=1)
 
         # train
         if emotionIdxes is not None:
             emotionIdxes = emotionIdxes.transpose(0, 1)
-            return -self.CRFlayer(crfEmissions, emotionIdxes, mask=sentencesMask) + self.lossFunc(emissions.view(-1, self.numClasses), emotionIdxes.view(-1))
+            return -self.CRFlayer(crfEmissions, emotionIdxes, mask=sentencesMask) + self.lossFunc(
+                emissions.view(-1, self.numClasses), emotionIdxes.view(-1))
         else:
             return self.CRFlayer.decode(crfEmissions, mask=sentencesMask)
